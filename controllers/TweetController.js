155d54a5cd6ddd82f58a08acd0630f7734f1f8d0
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-10-03 21:54:59 -0300

    first commit

---
+++ controllers/TweetController.js
@@ -0,0 +1,46 @@
+const Tweet = require('../models/Tweet')
+
+module.exports = {
+    async retrieve(req,res){
+        console.log("retrieve tweet called")
+
+        try {
+            // get a random document index
+            // const count = await Tweet.countDocuments()
+            // const randomTweet = Math.floor(Math.random() * count)
+            const tweet = await Tweet.findOne().sort('evals_count')
+
+            if (tweet == null){
+                return res.json({status:'error', message:`couldn't fetch tweets, database seems empty`})
+            }
+    
+            return res.json({status:'success', tweet: tweet})
+        } catch (error) {
+            console.error(error)
+            return res.json({status:'error', message:`an error occurred in the server`})
+        }
+
+    },
+    async evaluate (req,res) {
+        console.log(req.params)
+        const user_evaluation = req.params.evaluation
+        const tweet_id = req.params.id
+
+        if ( !['neutral','positive','negative'].includes(user_evaluation) ) {
+            return res.json({status:'error', message:'invalid evaluation string'})
+        }
+
+        try {
+            const tweet = await Tweet.findById(tweet_id)
+            
+            tweet.sentiments[user_evaluation] += 1
+            tweet.evals_count += 1
+            await tweet.save()
+
+            return res.json({status:'success', message:`evaluation has been saved`, counts: tweet.sentiments})    
+        } catch (error) {
+            console.error(error)
+            return res.json({status:'error', message:`an error occurred in the server`})
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+const server = require('http').Server(app)
+
+mongoose.connect(
+    process.env.MONGO_URL, {
+    useNewUrlParser: true
+})
+
+app.use(cors());
+
+app.use(require('./routes'))
+
+server.listen(process.env.PORT || 3333)
